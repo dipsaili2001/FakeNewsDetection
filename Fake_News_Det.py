f47361832606afa47598c2b5c1928c9dfa23b30d
Dipesh Saili <54875003+dipsaili2001@users.noreply.github.com>
2022-07-03 01:57:00 +0530

    Update Fake_News_Det.py

--- Fake_News_Det.py
+++ Fake_News_Det.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import PassiveAggressiveClassifier
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
